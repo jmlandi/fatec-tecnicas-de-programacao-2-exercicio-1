25a2d85c80606acc176e081ef84aea55f9e0fdac
Landi <joaomarcospsnbr@gmail.com>
2025-09-09 20:09:05 -0300

    EX005

--- src/main/java/org/example/ex005/Main.java
+++ src/main/java/org/example/ex005/Main.java
@@ -1,4 +1,26 @@
 package org.example.ex005;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número para descobrir se é primo ou não: ");
+        int numberToCheck = scanner.nextInt();
+        int numberOfDivisions = 0;
+
+        for (int i = 1; i <= numberToCheck; i++) {
+            if (numberToCheck % i == 0) numberOfDivisions++;
+        }
+
+        if (numberOfDivisions > 2 || numberOfDivisions == 1) {
+            System.out.println("O número não é primo.");
+        } else {
+            System.out.println("O número é primo.");
+        }
+
+    }
+
 }
